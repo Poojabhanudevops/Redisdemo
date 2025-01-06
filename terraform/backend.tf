f7e298804b8c876a5ba75a06e15525bc6bad9307
Poojabhanudevops <poojabhanu16@gmail.com>
2025-01-06 18:16:49 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-bucket-a"   # Replace with your S3 bucket name
+    bucket         = "devopsninja28"   # Replace with your S3 bucket name
     key            = "terraform/terraform.tfstate"      # Path within the bucket
     region         = "ap-south-1"               # Replace with your AWS region
     encrypt        = true
